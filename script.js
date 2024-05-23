0201d2ec119be2884ddccca8f67defc8c0e49fe8
coconut-exe <152046489+coconut-exe@users.noreply.github.com>
2024-05-23 15:24:34 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const button = document.getElementById('myButton');
     const dynamicMessage = document.getElementById('dynamicMessage');
 
-    // 模拟 URL 中的查询参数
-    const source = 'ads'; // 可以手动设置为 'email', 'social', 'ads' 等
+    // 获取 URL 中的查询参数
+    const urlParams = new URLSearchParams(window.location.search);
+    const source = urlParams.get('source');
 
     // 根据不同的 source 参数显示不同的消息
     switch (source) {
